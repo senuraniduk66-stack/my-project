f86dcb220a2dccf97810de86c3d6a7d1c8b5e50e
senuraniduk66-stack <senuraniduk66@gmail.com>
2026-03-06 00:42:59 +0530

    web project

---
+++ web.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>My Small Python Website</h1>
+    <p>Welcome to my web application.</p>
+    <a href='/about'>Go to About Page</a>
+    """
+
+@app.route("/about")
+def about():
+    return """
+    <h2>About</h2>
+    <p>This is a simple website made using Python and Flask.</p>
+    <a href='/'>Back to Home</a>
+    """
+
+if __name__ == "__main__":
+    app.run(debug=True)
